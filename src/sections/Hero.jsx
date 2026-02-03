89b52187954ba00948ebd718a27ee1c55c072dc1
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-02-03 12:15:31 +0100

    new file animatedborder and mod hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 
-import { ArrowRight, Download } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import { Button } from "../components/Button";
+import { AnimatedBorderButton } from "../components/AnimatedBorderButton";
 
 export const Hero = () =>{
     return (
@@ -67,31 +68,7 @@ export const Hero = () =>{
                             <Button size="lg">
                                 Contact Me <ArrowRight className="w-5 h-5"/>
                             </Button>
-                            <button>
-                                {/* Animated SVG Border */}
-                            <svg
-                                className="absolute left-0 top-0 w-full h-full pointer-events-none download-cv-border"
-                                viewBox="0 0 200 60"
-                                preserveAspectRatio="none"
-                                style={{ overflow: "visible" }}
-                            >
-                                <path
-                                    d="M 30,1 A 29,29 0 0 0 1,30 L 1,30 A 29,29 0 0 0 30,59 L 170,59 A 29,29 0 0 0 199,30 L 199,30 A 29,29 0 0 0 170,1 Z"
-                                    fill="none"
-                                    stroke="var(--color-primary)"
-                                    strokeWidth="2"
-                                    strokeDasharray="400 550"
-                                    strokeDashoffset="400"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="animated-border-path"
-                                    />
-                                </svg>
-                                <span>
-                                    <Download/>
-                                    Download CV
-                                    </span>
-                            </button>
+                            <AnimatedBorderButton/>
                         </div>
                     </div>
                     {/* Right Column - Text Content */}
