797175792b1126361f8482c60f11a74b61a82f63
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-02-05 19:47:45 +0100

    addind timeline section

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -77,7 +77,22 @@ export const Experience = () =>{
 
                     {/* Experience Items*/}
                     <div className="space-y-12">
-                        
+                        {experiences.map((exp,idx) => (
+                            <div
+                            key={idx}
+                            className="relative grid md:grid-cols-2 gap-8 animate-fade-in"
+                            style={{animationDelay: `${(idx + 1) * 150}ms`}}
+                            >
+                                {/* Timeline Dot */}
+                                <div className="absolute left-0 md:left-1/2 top-0 w-3 h-3 bg-primary rounded-full -translate-x-1/2 ring-4 ring-background z-10">
+                                    {
+                                        exp.current &&(
+                                            <span className="absolute inset-0 rounded-full bg-primary animate-ping opacity-75"/>
+                                        )
+                                    }
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
