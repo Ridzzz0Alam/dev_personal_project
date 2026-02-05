460bba45f709d6083baae93b3550ffeb6083825d
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-02-05 18:58:18 +0100

    started with expereince section

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,3 +1,19 @@
 export const Experience = () =>{
-    return <section></section>
+    return(
+        <section>
+            <div className="absolute top-1/2 left-1/4 w-96
+            h-96 bg-primary/5 rounded-full blur-3xl -translate-y-1/2
+            "/>
+
+            <div className="container mx-auto px-6 relative z-10">
+                {/* Section Header*/}
+                <div className="max-w-3xl mb-16">
+                    <span className="text-secondary-foreground text-sm
+                    font-medium tracking-wider uppercase animate-fade-in">
+                        Career Journey
+                    </span>
+                </div>
+            </div>
+        </section>
+    )
 }
