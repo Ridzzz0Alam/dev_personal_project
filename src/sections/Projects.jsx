28a5592ce037fe77fb02067c3049e520c9b8516f
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-02-05 10:51:59 +0100

    started projects section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,3 +1,26 @@
 export const Projects = () =>{
-    return <section></section>
+    return (
+        <section>
+            {/* Bg Glows */}
+            <div>
+                <div>
+                    <div>
+                        {/* Section Header */}
+                        <div>
+                            <span>
+                                Featured Work
+                            </span>
+                            <h2>
+                                Projects that
+                                <span>
+                                    {" "}
+                                    make an impact 
+                                </span>
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
