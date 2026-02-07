203687d8fcf9f0fc76ce2ecfa59d1ddd95599015
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-02-07 16:01:23 +0100

    adding logos

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,11 +1,24 @@
+import { Github, Linkedin, Twitter } from "lucide-react"
 
+const socialLinks = [
+    {icon: Github, href: "#", label: "GitHub"},
+    {icon: Linkedin, href: "#", label: "Linkedin"},
+    {icon: Twitter, href: "#", label: "Twitter"},
+];
+
+const footerLinks = [
+    {href: "#about", label: "About"},
+    {href: "#projects", label: "Projects"},
+    {href: "#experience", label: "Experience"},
+    {href: "#contact", label: "Contact"},
+]
 
 export const Footer = () =>{
     return(
         <footer>
             <div>
                 <div>
-                    
+
                 </div>
             </div>
         </footer>
